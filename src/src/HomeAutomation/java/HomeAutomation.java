1aeada948e450d37f9c0c540a2f8de36089d6ff9
phantuannhat <59288542+phantuannhat@users.noreply.github.com>
2021-09-07 14:28:07 +0700

    new praise

--- src/src/HomeAutomation/java/HomeAutomation.java
+++ src/src/HomeAutomation/java/HomeAutomation.java
@@ -3,5 +3,6 @@ package src.HomeAutomation.java;
 class HomeAutomation{
   public static void main(String[] args) {
     System.out.println("Welcome to my Home");
+    System.out.println("Well Amazing");
   }
 }
